f02471621acbc30ed5261a49a7981c1f07de8242
Radek Goblin Pieczonka <goblin@pentex.pl>
2017-04-04 10:48:50 +0200

    add dependencies, bump to 0.1.2 - fixes #1

--- setup.py
+++ setup.py
@@ -3,14 +3,20 @@ from setuptools import setup
 setup(
     name     = 'bunk',
     packages = ['bunk',],
-    version  = '0.1',
+    version  = '0.1.2',
     description = 'templating and environment management for kubernetes manifests',
     license  = 'MIT',
     scripts  = ['bin/bunk',],
     author   = 'Radek Goblin Pieczonka',
     author_email = 'goblin@pentex.pl',
     url = 'https://github.com/goblain/bunk',
-    download_url = 'https://github.com/goblain/bunk/tarball/0.1.tar.gz',
+    download_url = 'https://github.com/goblain/bunk/tarball/0.1.2.tar.gz',
     keywords = ['kubernetes', 'templates', 'environment'],
-    classifiers = []
+    classifiers = [],
+    install_requires=[
+      'PyYAML',
+      'boto',
+      'Jinja2',
+      'hvac'
+    ]
 )
